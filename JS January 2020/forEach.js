eccd47df635d69eb6db2bce81c0c577d166fa465
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-12 15:57:17 +0545

    array forEach meethod to print elements and their index

---
+++ JS January 2020/forEach.js
@@ -0,0 +1,6 @@
+// arr.foreach(callback(currentValue, index, array))
+
+const animals = ['cat', 'dog', 'monkey', 'squirrel']
+
+animals.forEach((value, index) => 
+console.log(`Number ${index} is ${value}`));

fb0b3f0c4e57d625bc4e2c17b95b7ff012cc8ab2
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-20 14:38:12 +0545

    Extracted object properties and values

--- Object Oriented JavaScript/object-methods.js
+++ Object Oriented JavaScript/object-methods.js
@@ -14,3 +14,16 @@ const tree = {
   // object can access property of its own using this keyword
   tree.identify();
   console.log(tree.growOneFoot());
+
+
+// Extracting properties and values of objects
+  const triangle = {
+    type: 'polygon',
+    sides: 3,
+    sumOfAngles: 180,
+    equilateral: true,
+    equiangular: true
+  };
+
+console.log(Object.keys(triangle));    //  [ 'type', 'sides', 'sumOfAngles', 'equilateral', 'equiangular' ]
+console.log(Object.values(triangle));  //  [ 'polygon', 3, 180, true, true ]

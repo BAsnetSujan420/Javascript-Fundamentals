70188b57ad833fef29d9960329a5dde761959893
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-21 17:51:30 +0545

    Invoked function with argument and object to called upon with call and apply

---
+++ Object Oriented JavaScript/invoke-object-function.js
@@ -0,0 +1,30 @@
+//call(), apply() and bind() is a method directly invoked onto a function.
+const mockingbird = {
+    title: 'To Kill a Mockingbird',
+    describe: function () {
+      console.log(`${this.title} is a classic novel`);
+    }
+  };
+
+mockingbird.describe();
+// 'To Kill a Mockingbird is a classic novel'
+
+const pride = {
+    title: 'Pride and Prejudice'
+  };
+  
+mockingbird.describe.call(pride);
+// 'Pride and Prejudice is a classic novel'
+
+
+const andrew = {
+    name: 'Andrew'
+  };
+  
+function introduce(one, two) {
+  console.log(`I'm ${this.name} and my favorite programming language is ${one} and ${two}.`);
+}
+
+//apply() is similar to call but it can accept multiple arguments
+introduce.apply(andrew, ['Javascript', 'Python']);
+

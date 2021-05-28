60e258e01b8494a8ca747736d5133f2be3b329fc
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-28 18:53:02 +0545

    Template literals preserves the text format

---
+++ ES6/Syntax/template-literals.js
@@ -0,0 +1,19 @@
+const student = {
+    name: "Max Cave",
+    guardian: "Terry Cave"
+};
+
+const teacher = {
+    name: "Chris Brook",
+    subject: "Parkour"
+}
+
+let note = `${teacher.name},
+
+Please excuse ${student.name}.
+He is recovering from the leg sprain.
+
+Thank You,
+${student.guardian}`;
+
+console.log(note);

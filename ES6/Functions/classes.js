741a61c7ae52711cfcd4e02eea9fcd62ce49413b
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-28 21:09:29 +0545

    Class is function implemented with prototypal inheritance

--- ES6/Functions/classes.js
+++ ES6/Functions/classes.js
@@ -11,8 +11,23 @@ class Plane {
     }
   }
 
+//   ES6 classes are just a mirage and hide the fact that prototypal inheritance 
+  function Plane2(numEngines) {
+    this.numEngines = numEngines;
+    this.enginesActive = false;
+  }
+  
+  // methods "inherited" by all instances
+  Plane2.prototype.startEngines = function () {
+    console.log(`Starting ${this.numEngines} engines.`);
+    this.enginesActive = true;
+  };
+  
+
   var richardsPlane = new Plane(2);
   richardsPlane.startEngines();
   
-  var jamesPlane = new Plane(4);
+  var jamesPlane = new Plane2(4);
   jamesPlane.startEngines();
+
+  console.log(typeof Plane);

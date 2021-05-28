fb20f2f4c2c0639951791770b737ee88e69c8d84
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-28 21:20:44 +0545

    Added each property of object as uinque with Symbol

---
+++ ES6/Built-ins/symbols.js
@@ -0,0 +1,19 @@
+const bowl = {
+    'apple': { color: 'red', weight: 136.078 },
+    'banana': { color: 'yellow', weight: 183.151 },
+    'orange': { color: 'orange', weight: 170.097 },
+    'banana': { color: 'yellow', weight: 176.845 }
+  };
+
+  console.log(bowl);
+
+// A Symbol is unique and immutable data type used to identify unique properties of object 
+
+const myBowl = {
+    [Symbol('apple')]: { color: 'red', weight: 136.078 },
+    [Symbol('banana')]: { color: 'yellow', weight: 183.15 },
+    [Symbol('orange')]: { color: 'orange', weight: 170.097 },
+    [Symbol('banana')]: { color: 'yellow', weight: 176.845 }
+  };
+
+  console.log(myBowl);

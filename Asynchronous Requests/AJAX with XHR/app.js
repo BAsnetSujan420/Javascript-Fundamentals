96f57a23779ddb0f8933490dbb158ff4178acc82
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-29 21:51:56 +0545

    Loaded articles using New York Times API with XHR

--- Asynchronous Requests/AJAX with XHR/app.js
+++ Asynchronous Requests/AJAX with XHR/app.js
@@ -36,5 +36,31 @@ function addImage(){
      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
  }
 
+ const articleRequest = new XMLHttpRequest();
+articleRequest.onload = addArticles;
+articleRequest.onerror = function(err){
+    requestError(err, 'article');};
+
+articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<your-API-key-goes-here>`);
+articleRequest.send();
+
+
+function addArticles () {
+    let htmlContent = '';
+    const data = JSON.parse(this.responseText);
+
+    if (data && data.response.docs && data.response.docs.length > 1) {
+        htmlContent = '<ul>' + data.response.docs.map(article + `<li class="article">
+        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+        <p>${article.snippet}</p>
+        </li>`).join('') + '</ul>'
+            
+    } else {
+        htmlContent = '<div class="error-no-articles">No articles found.</div>'
+    }
+
+    responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+}
+
 })();
 

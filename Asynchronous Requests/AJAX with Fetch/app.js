83fba75be8f8bd358909fb129d8253693ef56fcc
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-05-29 22:06:22 +0545

    Loaded articles using New York Times API with Fetch API

--- Asynchronous Requests/AJAX with Fetch/app.js
+++ Asynchronous Requests/AJAX with Fetch/app.js
@@ -33,4 +33,28 @@ function addImage(data) {
         htmlContent = '<div class="error-no-image">Unfortunately, no image was returned for your search.</div>'
     }
 }
+
+fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<your-API-key-goes-here>`)
+.then(function(response) {
+    return response.json();
+}).then(addArticles)
+.catch(e => requestError(e, 'articles'));
+
+function addArticles () {
+    let htmlContent = '';
+
+    if (data && data.response.docs && data.response.docs.length > 1) {
+        const articles = data.response.docs;
+        htmlContent = '<ul>' + articles.map(article + `<li class="article">
+        <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+        <p>${article.snippet}</p>
+        </li>`).join('') + '</ul>'
+            
+    } else {
+        htmlContent = '<div class="error-no-articles">No articles found.</div>'
+    }
+
+    responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+}
+
 })();

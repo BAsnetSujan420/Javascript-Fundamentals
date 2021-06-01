4c2bf3a9582a67f231393b695176c043aa28beae
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-06-01 17:40:43 +0545

    Regular expression search, match and replace example

--- Regular Expressions/regex-cc.js
+++ Regular Expressions/regex-cc.js
@@ -22,3 +22,20 @@ const test3 = rg1.test(str3);  // true
 const test4 = rg2.test(str3);  // true
 const test5 = rg2.test(str4);  // false
 const test6 = rg3.test(str4);  // true
+
+// search return index when found
+const test7 = str3.search(rg2);
+console.log(test7);             // 6
+
+
+// match returns array of matched string and group
+const test8 = str3.match(rg2);
+console.log(test8);            //  [ 'world', index: 6, input: 'hello world', groups: undefined ]
+
+const test9 = str4.match(rg4);
+console.log(test9);            // [ 'Hello World','Hello', 'World', index: 0,input: 'Hello World',groups: undefined ]
+
+// replace method
+const test10 = str5.replace(rg5, "codeSTACKr");
+console.log(test10);  // codeSTACKr is Awesome!
+console.log(str5);    // YouTube is Awesome!

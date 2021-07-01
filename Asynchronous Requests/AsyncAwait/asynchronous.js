9afa75d607cd14013aafcf7e9a317877641c62ed
BAsnetSujan420 <basnetsujan1828420@gmail.com>
2021-07-01 17:18:50 +0545

    Asychronous example with setTimeout Web API

---
+++ Asynchronous Requests/AsyncAwait/asynchronous.js
@@ -0,0 +1,14 @@
+// asynchronous example
+
+console.log("I ");
+
+console.log("eat ");
+
+// setTimeout is Web API
+setTimeout(() => {
+  console.log("icecream ");
+}, 4000);
+
+console.log("with a ");
+
+console.log("spoon ");
